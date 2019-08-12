51dc0a1a1cb6b3e53f3abdb480a718ddde6f99a7
Zac Steinberg <zstein1234@gmail.com>
2019-08-12 18:50:10 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,5 @@ first_number = 55
 second_number = 34
 sum = first_number + second_number
 difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
