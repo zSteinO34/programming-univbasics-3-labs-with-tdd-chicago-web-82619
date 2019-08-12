9a0bb7b820dbc368155930328369d2d1c4b3a83f
Zac Steinberg <zstein1234@gmail.com>
2019-08-12 18:50:26 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
 # Add your variables here
+first_number = 55
+second_number = 34
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
